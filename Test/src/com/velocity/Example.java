9bad76877cde69554aec41656792aff51ec8b516
Anand <ananddanai2412@gmail.com>
2023-04-26 18:55:32 +0530

    Remote repo

--- Test/src/com/velocity/Example.java
+++ Test/src/com/velocity/Example.java
@@ -3,7 +3,8 @@ package com.velocity;
 public class Example {
 	
 	public static void main(String[] args) {
-		System.out.println("10");
+		System.out.println("10");  
+		System.out.println("1000");
 	}
 
 }
